b63e603f348603fef04cf3aad0f5edc1918c3cb5
Wasan Aljohani <wasanalmrwani@hotmail.com>
2024-09-29 07:44:21 -0700

    adding levels page

---
+++ Team11Presentme/LevelsPage.swift
@@ -0,0 +1,18 @@
+//
+//  LevelsPage.swift
+//  Team11Presentme
+//
+//  Created by Wasan Aljohani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct LevelsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelsPage()
+}
