57e7060a05ebab41b0dc4ee2d883104389b4a18b
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-29 15:21:13 +0300

    Initial Commit

---
+++ Team11PresentmeTests/Team11PresentmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team11PresentmeTests.swift
+//  Team11PresentmeTests
+//
+//  Created by Haya Albaker on 29/09/2024.
+//
+
+import Testing
+@testable import Team11Presentme
+
+struct Team11PresentmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
