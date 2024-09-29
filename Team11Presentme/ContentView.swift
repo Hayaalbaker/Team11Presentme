b9fb9b5a08c9942dfe823d657c6c639981e02b9b
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-29 15:44:41 +0300

    Info about me

--- Team11Presentme/ContentView.swift
+++ Team11Presentme/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack() {
             Image("me")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 200, height: 200)
+                .frame(width: 300, height: 300, alignment: .center)
                 .clipShape(Circle())
+
             Text("Haya Albaker")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(.pink)
-            Text("Hi")
+
+            Text("I'm turning 21 on Oct 4! I love to design, travel, and learn new things.")
                  .font(.title2)
                  .foregroundColor(.gray)
             
